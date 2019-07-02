7817f473e900ecade5701306f8c3396e555f0c7e
Trevahr Robertson <Trevahr@gmail.com>
2019-07-02 11:24:25 -0400

    done

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,60 +1,56 @@
-
+require 'pry'
 
 class Artist
-
-
     attr_accessor :name, :songs, :add_song
 
     @@all = []
 
     def initialize(name)
         @name = name
-        @all = []
+        @all =[]
         @@all << self
     end
-    
+
     def add_song(song)
-            @all << song
+        @all << song
     end
 
     def songs
-        @all  
-        # binding.pry
+        @all
     end
 
-    def self.all
-        # binding.pry
-        @@all
-    end
-
-    def save 
+    def save
         @@all << self
     end
 
-    def self.find_or_create_by_name(names)
-        a = []
-        
-        if self.all.select do |artist|
-            artist.name == names
-            a = artist
-            end
-            a
-        else
-            Artist.new(names)
-        end
+    def self.all
+        @@all
     end
 
-    def self.add_song(song)
-        @@all << song
+    def self.find_or_create_by_name(artist_name)
+        # binding.pry
+        if self.all.find {|artist| artist.name == artist_name} != nil
+            # binding.pry
+        self.all.find do |artist| 
+            # binding.pry
+            artist.name == self
+           artist.name
+            end
+        else
+       artist_name = Artist.new(artist_name)
+    #    binding.pry
+    #   self.save
+       artist_name
+       
+        end
+        # binding.pry
     end
 
     def print_songs
-         
         songs.each do |song|
-          puts  song.name
-          end
-          
-        
+            puts song.name
+        end
     end
+
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,40 +1,23 @@
-
+require 'pry'
 
 class Song
-
+    
     attr_accessor :name, :artist
-
-   
 
     def initialize(name)
         @name = name
-        # binding.pry
     end
 
-    def self.new_by_filename(new_file)
-        # binding.pry
-        # song_info = new_song.gsub(/.mp3/, "").split(" - ")
-         writer, song_name = new_file.split(" - ")
-          new_song = self.new(song_name)
+
+    def self.new_by_filename(file_name)
+         artist, song_name = file_name.split(" - ")
+         new_song = self.new(song_name)
         #   binding.pry
-        #   new_song.artist = writer
-            # binding.pry
-        #   Artist.find_or_create_by_name(song.name)
-        #   binding.pry
-        #   new_song.artist_name = writer
+          artist1 = Artist.find_or_create_by_name(artist)
+          new_song.artist = artist1
+          artist1.add_song(new_song)
+          new_song
         # binding.pry
-        # Artist.add_song(song)
-        new_song
-        #Artist:0x00007fe9b8a52068
-            # binding.pry
-            
     end
-
-     def artist_name=(name)
-        binding.pry
-       Artist.find_or_create_by_name(name)
-    #    binding.pry
-     end
-
 
 end
